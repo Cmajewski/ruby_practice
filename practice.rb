692a0bca9f24b37302dc8ed3c8c9018ab3fc9a3e
github username <github email address>
2021-04-26 07:48:41 -0400

    add bundler and pry

--- practice.rb
+++ practice.rb
@@ -1,4 +1,15 @@
+require "pry"
+
 def dictionary(string,dictionary)
-    
+   new_string=string.split(" ")
+   new_string.each do |word|
+    if dictionary.include?(word)
+        return true
+    else
+        return false
+   end
+end
 
 end
+
+dictionary("Practice makes perfect",["practice","perfect","makes"])
